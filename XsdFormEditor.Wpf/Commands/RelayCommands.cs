0b24a4bdd4dc3b3e15dcbcfcc75beff3fafad6c5
Jan Stafa <honza.stafa@gmail.com>
2012-07-06 14:44:13 +0100

    Structure for MVVM pattern.

---
+++ XsdFormEditor.Wpf/Commands/RelayCommands.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Pex.Framework;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace XsdFormEditor.Wpf.Commands
+{
+    [TestClass, PexClass]
+    public partial class RelayCommands
+    {
+
+    }
+}
